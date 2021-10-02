901a4c646c11821446900368208f8bf8b0b5b104
Kyle Farwell <m@kfarwell.org>
2021-10-02 08:06:26 -0400

    shell: cd, pwd, mkdir, ls, history

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ function spawnTerm() {
         title: 'GelatoTerm',
         x: '44',
         y: '100',
-        width: '400',
-        height: '300',
+        width: '600',
+        height: '400',
         onresize: termFit.fit(), // doesn't work
         html: '<div class="term"></div>'
     });
@@ -21,28 +21,133 @@ function spawnTerm() {
     termFit.fit();
     term.write("root> ");
     term.focus();
-    var inputString = "";
+
+    var input = "";
+    var history = [];
+    var historyIndex = 0;
+    var pwd = "/";
+
     term.onKey(e => {
         if (e.key.charCodeAt(0) == 127) {
+            // Backspace
             term.write('\b\x1b[1;P');
-            inputString = inputString.slice(0,inputString.length - 1);
+            input = input.slice(0, input.length - 1);
+        }
+        else if (e.key == '\x1b[A' && history.length > 0) {
+            // Up arrow
+            historyIndex = Math.max(historyIndex - 1, 0);
+            input = history[historyIndex];
+            term.write('\x9B2K\rroot> ' + input);
+        }
+        else if (e.key == '\x1b[B') {
+            // Down arrow
+            historyIndex++;
+            if (historyIndex < history.length) {
+                input = history[historyIndex];
+                term.write('\x9B2K\rroot> ' + input);
+            } else {
+                historyIndex = history.length;
+                input = "";
+                term.write('\x9B2K\rroot> ');
+            }
+        }
+        else if (e.key == '\r') {
+            term.write('\r\n');
+            args = input.split(' ');
+            cmd = args.shift();
+
+            switch(cmd) {
+                case "help":
+                    term.write("there is no help, only gelato\n");
+                    break;
+
+                case "cd":
+                    if (args.length == 0) {
+                        pwd = "/";
+                    } else if (args.length == 1) {
+                        pwd = traversePath(pwd, args[0].split("/"));
+                    } else {
+                        term.write("cd: too many arguments\n");
+                    }
+                    break;
+
+                case "pwd":
+                    term.write(pwd + "\n");
+                    break;
+
+                case "mkdir":
+                    for (var i = 0; i < args.length; i++) {
+                        var dir = traversePath(pwd, args[i].split("/"));
+                        var parent = localStorage.getItem(dir.substr(0, dir.lastIndexOf("/")).replace(/^$/, '/'));
+
+                        if (parent == "d") {
+                            localStorage.setItem(dir, "d");
+                        } else if (parent == "f") {
+                            term.write("mkdir: cannot create directory '" + args[i] + "': Not a directory\n");
+                        } else {
+                            term.write("mkdir: cannot create directory '" + args[i] + "': No such file or directory\n");
+                        }
+                    }
+                    break;
+
+                case "ls":
+                    var files = [];
+                    for (var i = 0; i < localStorage.length; i++) {
+                        files.push(localStorage.key(i));
+                    }
+                    files.sort();
+                    for (var i = 0; i < files.length; i++) {
+                        var file = files[i];
+                        var parent = file.substr(0, file.lastIndexOf("/")).replace(/^$/, '/');
+                        if (parent == pwd && parent != file) {
+                            term.write(file.substr(file.lastIndexOf("/") + 1) + "\n\r");
+                        }
+                    }
+                    break;
+
+                case "history":
+                    for (var i = 0; i < history.length; i++) {
+                        term.write(history[i] + "\n\r");
+                    }
+                    break;
+
+                case "":
+                    break;
+
+                default:
+                    term.write(cmd + ": command not found. If you are lost, type 'help'\n")
+            }
+
+            term.write("\rroot> ");
+            if (input.length > 0) {
+                history.push(input);
+                historyIndex = history.length;
+            }
+            input = "";
         }
         else {
-            inputString += e.key;
+            input += e.key;
             term.write(e.key);
         }
-        if (e.key == '\r') {
-            term.write('\n');
-            if (inputString.toLowerCase().startsWith("help")) {
-                term.write("there is no help, only gelato");
-            }
-            else {
-                term.write("Unrecognized Command. If you are lost, type 'help'")
-            }
-            term.write('\n\rroot> ');
-            inputString = "";
-        }
     });
+}
+
+function traversePath(pwd, path) {
+    if (path.length == 0) {
+        return pwd.replace(/\/+/g, '/').replace(/^$/, '/');
+    }
+    else if (path[0] == "") {
+        return traversePath("/", path.slice(1));
+    }
+    else if (path[0] == "..") {
+        return traversePath(pwd.substr(0, pwd.lastIndexOf("/")), path.slice(1));
+    }
+    else if (path[0] == ".") {
+        return traversePath(pwd, path.slice(1))
+    }
+    else {
+        return traversePath(pwd + "/" + path[0], path.slice(1));
+    }
 }
 
 function spawnBrowser() {
@@ -87,8 +192,6 @@ function toggleStart() {
 function updateClock() {
     document.getElementById('gde-clock').innerHTML = new Date().toLocaleTimeString();
 }
-updateClock();
-setInterval(updateClock, 1000);
 
 var slowText = function (message, index, interval) {   
     if (index < message.length) {
@@ -134,3 +237,8 @@ function commandHandler(input) {
         term.write("Unrecognized Command. If you are lost, type 'help'")
     }
 }
+
+updateClock();
+setInterval(updateClock, 1000);
+
+localStorage.setItem("/", "d");
